2f09768aa17bbffb6e6e6ecfcd74223e8ecd00ae
Jason Aicardi <jasonaicardi@gmail.com>
2016-04-26 19:51:23 -0400

    first online version

--- out/timer.js
+++ out/timer.js
@@ -1,45 +1,38 @@
+(function() {
+	var time = 1000;
+	var display = document.getElementById("timer");
 
-var time = 1000;
-var display = document.getElementById("timer");
+	function formatTime(t) {
+		function pl(testvar,name) {
+			return testvar + ' ' + name + (testvar!==1?'s':'');
+		}
+		var s = t/1000,
+		m = s/60|0,
+		s = s-m*60,
+		h = m/60|0,
+		m = m-h*60,
+		d = h/24|0,
+		h = h-d*24;
 
-function formatTime(t) {
-	function p(n) {
-		return (n<10?'0':'') + n;
+		return ['<div>'+pl(d,'day'),pl(h,'hour'),pl(m,'minute'),pl(s.toFixed(2),'second')+'</div>'].join('</div><div>');
 	}
 
-	
-	var ms = t % 1000,
-	s = (t-ms)/1000,
-	ms = p(ms/10|0),
-	m = s/60|0,
-	s = p(s-m*60),
-	h = m/60|0,
-	m = p(m-h*60),
-	d = h/24|0,
-	h = p(h-d*24);
+	function startTimer(endTime) {
+		var duration = endTime - (new Date).getTime();
+		timerTick(duration,endTime);
+	}
 
+	function timerTick(d,et) {
+		
+		var next = d-10;
+		var wait = et - (new Date).getTime() - next;
+		display.innerHTML = formatTime(d);
+		setTimeout(function()
+			{
+				timerTick(next,et);
+			}, (wait>0)?wait:0);
+	}
 
-	return [`<div>${d} days`,`${h} hours`,`${m} minutes`,`${s}.${ms} seconds`].join('</div><div>');
-}
+	startTimer((new Date(2016,5,24,17)).getTime());
 
-function updateTimer(time) {
-	display.innerHTML = formatTime(time);
-}
-
-function startTimer(endTime) {
-	var duration = endTime - (new Date).getTime();
-	timerTick(duration,endTime);
-}
-
-function timerTick(d,et) {
-	
-	var next = d-10;
-	var wait = et - (new Date).getTime() - next;
-	updateTimer(d);
-	setTimeout(function()
-		{
-			timerTick(next,et);
-		}, (wait>0)?wait:0);
-}
-
-startTimer((new Date(2016,5,24,17)).getTime());
+})();
