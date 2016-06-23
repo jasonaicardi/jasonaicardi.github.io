5e2562d1f6c2d97210b619ca004db4154bd95aac
Jason Aicardi <jasonaicardi@gmail.com>
2016-06-23 17:50:20 -0400

    webworkers do the lifting so it runs correctly in the background

--- sayonara/timer.js
+++ sayonara/timer.js
@@ -2,37 +2,13 @@
 	var time = 1000;
 	var display = document.getElementById("timer");
 
-	function formatTime(t) {
-		function pl(testvar,name) {
-			return testvar + ' ' + name + (testvar!==1?'s':'');
-		}
-		var s = t/1000,
-		m = s/60|0,
-		s = s-m*60,
-		h = m/60|0,
-		m = m-h*60,
-		d = h/24|0,
-		h = h-d*24;
+	var worker = new Worker('worker.js')
 
-		return ['<div>'+pl(d,'day'),pl(h,'hour'),pl(m,'minute'),pl(s.toFixed(2),'second')+'</div>'].join('</div><div>');
-	}
+	worker.addEventListener('message', function(e) {
+		display.innerHTML = e.data;
+	}, false);
 
-	function startTimer(endTime) {
-		var duration = endTime - (new Date).getTime();
-		timerTick(duration,endTime);
-	}
+	worker.postMessage((new Date(2016,5,24,17)).getTime());
 
-	function timerTick(d,et) {
-		
-		var next = d-10;
-		var wait = et - (new Date).getTime() - next;
-		display.innerHTML = formatTime(d);
-		setTimeout(function()
-			{
-				timerTick(next,et);
-			}, (wait>0)?wait:0);
-	}
-
-	startTimer((new Date(2016,5,24,17)).getTime());
 
 })();
---
+++ sayonara/worker.js
@@ -0,0 +1,36 @@
+function formatTime(t) {
+		function pl(testnum) {
+			return (testnum!=1?'s':'');
+		}
+		var s = t/1000,
+		m = s/60|0, s = (s-m*60).toFixed(2),
+		h = m/60|0, m = m-h*60,
+		d = h/24|0, h = h-d*24;
+
+		return ['<div>'+ 
+			d + ' day'+pl(d),
+			h + ' hour'+pl(h),
+			m +' minute'+pl(m),
+			s + ' second'+pl(s)
+			+'</div>'].join('</div><div>');
+}
+
+	
+function startTimer(endTime) {
+	var duration = endTime - (new Date).getTime();
+	timerTick(duration,endTime);
+}
+
+function timerTick(d,et) {
+		
+	var next = d-10;
+	var wait = et - (new Date).getTime() - next;
+	self.postMessage(formatTime(next));
+	setTimeout(function() {
+			timerTick(next,et);
+		}, (wait>0)?wait:0);
+}
+
+self.addEventListener('message', function(e) {
+	startTimer(e.data);
+}, false);
