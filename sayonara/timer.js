ea98df13fd28bd6aea4ae42e81457cc362b7d98b
Jason Aicardi <jasonaicardi@gmail.com>
2016-08-22 21:51:56 -0400

    anniversary message

--- sayonara/timer.js
+++ sayonara/timer.js
@@ -1,5 +1,5 @@
 (function() {
-	var time = 1000;
+
 	var display = document.getElementById("timer");
 
 	var worker = new Worker('worker.js')
@@ -7,8 +7,5 @@
 	worker.addEventListener('message', function(e) {
 		display.innerHTML = e.data;
 	}, false);
-
 	worker.postMessage((new Date(2016,5,24,17)).getTime());
-
-
 })();
