eac59cc1ec84ab0c563c057dcce3decc9daa83d4
Jason Aicardi <jasonaicardi@gmail.com>
2016-09-21 18:34:38 -0400

    good enough for now

---
+++ lex/script.js
@@ -0,0 +1,26 @@
+var request = new XMLHttpRequest(),
+	lines = [],
+	messagediv = document.getElementById("message"),
+	btn = document.getElementById("more");
+
+
+function newMessage(){
+	messagediv.innerHTML = lines[Math.floor(Math.random()*lines.length)];
+}
+
+function startTimer(){
+	setInterval(newMessage,6000);
+}
+
+
+request.onload = function() {
+	lines = this.responseText.split('\n');
+	btn.onclick = newMessage;
+	btn.disabled = false;
+	newMessage();
+	startTimer();
+}
+
+request.open('GET','list.txt',true);
+request.send();
+
