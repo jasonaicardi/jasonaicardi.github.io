b98110f540796e6269049e55cbb223f0924f8a34
Jason Aicardi <jasonaicardi@gmail.com>
2016-10-02 19:16:14 -0400

    changed the structure of script.js to reset count when 'next' is pressed and allow for pause and play buttons, added pause/play (auto-changing) and next buttons

--- lex/script.js
+++ lex/script.js
@@ -1,22 +1,52 @@
 var request = new XMLHttpRequest(),
 	lines = [],
+	running = false,
+	id = 0,
 	messagediv = document.getElementById("message"),
-	btn = document.getElementById("more");
+	nextBtn = document.getElementById("next"),
+	playpauseBtn = document.getElementById("playpause");
 
 
 function newMessage(){
 	messagediv.innerHTML = lines[Math.floor(Math.random()*lines.length)];
 }
 
-function startTimer(){
-	setInterval(newMessage,9000);
+function toggle(){
+	if(running){
+		stopTimer();
+	} else {
+		startTimer();
+	}
 }
 
+function nextMsg(){
+	newMessage();
+	if(running) startTimer();
+}
+
+function startTimer(){
+	clearInterval(id);
+	id = setInterval(newMessage,6000);
+	running = true;
+	updateButton();
+}
+
+function stopTimer(){
+	clearInterval(id);
+	running = false;
+	updateButton();
+}
+
+function updateButton(){
+	playpauseBtn.firstChild.innerHTML = (running)?"pause":"play_arrow";
+}
 
 request.onload = function() {
 	lines = this.responseText.split('\n');
-	btn.onclick = newMessage;
-	btn.disabled = false;
+	nextBtn.onclick = nextMsg;
+	playpauseBtn.onclick = toggle;
+	nextBtn.disabled = false;
+	playpauseBtn.disabled = false;
 	newMessage();
 	startTimer();
 }
